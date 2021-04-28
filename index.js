5a7fcc74727a53aba667c140214b64a5a8e46d26
blukhi99 <blukhi99@outlook.com>
2021-04-28 11:17:20 +0530

    Integrate VUE

--- index.js
+++ index.js
@@ -1,12 +1,38 @@
 const cool = require('cool-ascii-faces');
-const express = require('express')
+const express = require('express'),
+      bodyParser = require("body-parser");
+
 const path = require('path')
 const PORT = process.env.PORT || 5000
 
+const { Pool } = require('pg');
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
+});
+
 express()
+  .use(bodyParser.json())
+  .use(express.static(path.join(__dirname, 'dist')))
   .use(express.static(path.join(__dirname, 'public')))
+
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
+  .get('/', (req, res) => res.sendFile(path.join(__dirname, 'dist/index.html')))
+
   .get('/cool', (req, res) => res.send(cool()))
+  .get('/db', async (req, res) => {
+    try {
+      const client = await pool.connect();
+      const result = await client.query('SELECT * FROM test_table');
+      const results = { 'results': (result) ? result.rows : null};
+      res.render('pages/db', results );
+      client.release();
+    } catch (err) {
+      console.error(err);
+      res.send("Error " + err);
+    }
+  })
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
